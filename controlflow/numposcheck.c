5166d50268a64d20819af5b03a2991bf740458b5
crazyboy252s <saich3215@gmail.com>
2026-09-23 11:16:58 +0530

    Add number positive negative checker

---
+++ controlflow/numposcheck.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+
+ int n;
+ printf("tner a number ");
+ scanf("%d",&n);
+ if (n>0)
+ {printf("the number you entered is postive ");}
+ if (n<0)
+ {printf("the number you entered is negative");}
+else 
+{printf("your number is zero ");}
+
+
+
+
+    return 0;
+     
+}
